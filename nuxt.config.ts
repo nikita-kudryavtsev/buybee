623c9d346f7282436862f4d941cc580862342162
Nikita Kuryavtsev <Nikita.Kudryavcev@innostage-group.ru>
2024-11-17 21:02:59 +0300

    add prisma orm, jwt, bcrypt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxtjs/color-mode"],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxtjs/color-mode", "@prisma/nuxt"],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
